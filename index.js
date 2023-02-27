65bf0cdebfbf6790846aba9162729baceb2883a4
Ishan <udgirkarishan@gmail.com>
2023-02-27 18:46:51 +0530

    Q1

---
+++ index.js
@@ -0,0 +1,16 @@
+// Q1 Write an ES6 function that accepts an array of integers and returns the maximum element in the array.
+
+const getMaxElement = arr => {
+    let maxElement = Math.min;
+    for(let i = 0 ; i < arr.length ; i++){
+     if(arr[i] > arr[i+1] && arr[i] > maxElement){
+         maxElement = arr[i]
+     }
+    }
+    return maxElement
+
+}
+
+let array = [4,78,8,3,6,0,12,34]
+console.log(getMaxElement(array))
+ 
